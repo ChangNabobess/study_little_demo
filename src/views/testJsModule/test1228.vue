02cf1ca5bea1921b9015d859a235cdc02d0f6c0f
yifan.chang <yifan.chang@adicon.com.cn>
2024-03-29 13:40:53 +0800

    feat：代码提交

--- src/views/testJsModule/test1228.vue
+++ src/views/testJsModule/test1228.vue
@@ -9,35 +9,35 @@
 export default {
   data() {
     return {
-      tranformArr: [5,3,2,9,4,6,8,1,7]
+      tranformArr: [5, 3, 2, 9, 4, 6, 8, 1, 7]
     };
   },
   computed: {},
   watch: {},
   methods: {
-    arrSort(){
-      console.log(this.tranformArr.sort((a,b) => b-a));
+    arrSort() {
+      console.log(this.tranformArr.sort((a, b) => b - a));
     },
-    observeFun(){
-      let data = {name: '张三'}
+    observeFun() {
+      let data = { name: '张三' }
       let observeArr = []// 观察者数组
-      let addObserve = function(fun) { // 添加观察者的方法
+      let addObserve = function (fun) { // 添加观察者的方法
         observeArr.push(fun)
       }
       let dispatchObserve = (str) => { // 
         data.name = str
-        for( let i = 0; i<observeArr.length; i++) {
-            observeArr[i](str)
+        for (let i = 0; i < observeArr.length; i++) {
+          observeArr[i](str)
         }
       }
-      addObserve((str) =>{
-        console.log('第一个观察者',str)
+      addObserve((str) => {
+        console.log('第一个观察者', str)
       })
-      addObserve((str) =>{
-        console.log('第二个观察者',str)
+      addObserve((str) => {
+        console.log('第二个观察者', str)
       })
-       addObserve((str) =>{
-        console.log('第三个观察者',str)
+      addObserve((str) => {
+        console.log('第三个观察者', str)
       })
       dispatchObserve('畅一凡')
     },
@@ -45,6 +45,39 @@ export default {
       // this.$router.replace({
       //   path: '/countdown'
       // })
+    },
+    initWebScoket() {
+      // const webScoketEnty = new WebSocket('ws://localhost:4000/test');
+
+      // webScoketEnty.onopen = () => {
+      //   console.log('webScoket链接成功');
+      // }
+      // webScoketEnty.onmessage = (event) => {
+      //   console.log('webScoket链接成功',event.data);
+      // }
+      // webScoketEnty.onclose = () => {
+      //   console.log('webScoket关闭');
+      // }
+      // 
+      let ws = new WebSocket('ws://localhost:4000/test');
+
+      ws.onopen = function (evt) { // 连接建立触发
+        console.log('建立连接，状态:' + ws.readyState);
+        ws.send('hello Word');
+      };
+
+      ws.onmessage = function (evt) { // 服务端返回数据触发
+        // var data = JSON.parse(evt.data)
+        console.log("状态：" + ws.readyState + "；服务端返回数据:", evt);
+      };
+
+      ws.onerror = function (evt) { // 通信发生错误触发
+        console.log('发生错误，状态:' + ws.readyState);
+      };
+
+      ws.onclose = function (evt) { // 连接关闭触发
+        console.log("连接关闭，状态：", ws.readyState);
+      };
     }
   },
   created() {
@@ -52,11 +85,9 @@ export default {
     this.observeFun()
   },
   mounted() {
-
+    this.initWebScoket()
   },
 }
 </script>
 
-<style lang='scss' scoped>
-
-</style>
+<style lang='scss' scoped></style>
